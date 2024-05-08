f1a982fd11a3f5667ca8ec42662d7e01c4835822
Adam Gartee <agartee@gmail.com>
2024-05-08 08:08:31 -0400

    generated db migration and fixed account list page styles

---
+++ src/Accountater.Persistence.SqlServer/Migrations/20240508120605_AccountCascadeDelete.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accountater.Persistence.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AccountCascadeDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transaction_Account_AccountId",
+                table: "Transaction");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transaction_Account_AccountId",
+                table: "Transaction",
+                column: "AccountId",
+                principalTable: "Account",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transaction_Account_AccountId",
+                table: "Transaction");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transaction_Account_AccountId",
+                table: "Transaction",
+                column: "AccountId",
+                principalTable: "Account",
+                principalColumn: "Id");
+        }
+    }
+}
