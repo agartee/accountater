352785996928df0042b6d1d8fc475c6f0c8ecfc3
Adam Gartee <agartee@gmail.com>
2025-03-24 22:10:31 -0400

    cleanup

---
+++ src/Accountater.WebApp/ModelBinders/IEnumerableModelBinderProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Accountater.WebApp.ModelBinders
+{
+    public class IEnumerableModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        {
+            if (!context.Metadata.IsEnumerableType || context.Metadata.ModelType == typeof(string))
+                return null;
+
+            var elementType = context.Metadata.ElementType!;
+            var elementMetadata = context.MetadataProvider.GetMetadataForType(elementType);
+            var elementBinder = context.CreateBinder(elementMetadata);
+
+            var binderType = typeof(IEnumerableModelBinder<>).MakeGenericType(elementType);
+            return (IModelBinder)Activator.CreateInstance(binderType, elementBinder)!;
+        }
+    }
+}
