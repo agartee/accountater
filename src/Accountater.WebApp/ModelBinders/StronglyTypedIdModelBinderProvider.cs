7ed04b959e705f994ddc2dc569230ebdaaa8c86b
Adam Gartee <agartee@gmail.com>
2025-03-30 09:00:32 -0400

    updates to strongly typed id model binder

--- src/Accountater.WebApp/ModelBinders/StronglyTypedIdModelBinderProvider.cs
+++ src/Accountater.WebApp/ModelBinders/StronglyTypedIdModelBinderProvider.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace Accountater.WebApp.ModelBinders
 {
@@ -9,13 +11,26 @@ namespace Accountater.WebApp.ModelBinders
         {
             var modelType = context.Metadata.ModelType;
 
-            // Simple check: is it a readonly struct with a Guid constructor
-            if (modelType.IsValueType && modelType.GetConstructor(new[] { typeof(Guid) }) != null)
-            {
-                return new BinderTypeModelBinder(typeof(StronglyTypedIdModelBinder));
-            }
+            if (!modelType.IsValueType || modelType.IsPrimitive || modelType.IsEnum)
+                return null;
 
-            return null;
+            if (!modelType.IsLayoutSequential && !modelType.IsExplicitLayout && !modelType.IsDefined(typeof(StructLayoutAttribute)))
+                return null;
+
+            var hasSupportedCtor = modelType
+                .GetConstructors(BindingFlags.Public | BindingFlags.Instance)
+                .Any(c =>
+                {
+                    var parameters = c.GetParameters();
+                    return parameters.Length == 1 &&
+                           (parameters[0].ParameterType == typeof(Guid) ||
+                            parameters[0].ParameterType == typeof(string));
+                });
+
+            if (!hasSupportedCtor)
+                return null;
+
+            return new BinderTypeModelBinder(typeof(StronglyTypedIdModelBinder));
         }
     }
 }
