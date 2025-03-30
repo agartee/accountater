ede795937c0e2434de3a178ae1d169b0455fe8cf
Adam Gartee <agartee@gmail.com>
2025-03-30 09:40:06 -0400

    modelbinder fixes

--- src/Accountater.WebApp/ModelBinders/StronglyTypedIdModelBinderProvider.cs
+++ src/Accountater.WebApp/ModelBinders/StronglyTypedIdModelBinderProvider.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
-using System.Reflection;
 
 namespace Accountater.WebApp.ModelBinders
 {
@@ -10,19 +9,13 @@ namespace Accountater.WebApp.ModelBinders
         {
             var modelType = context.Metadata.ModelType;
 
-            if (!modelType.IsValueType || modelType.IsPrimitive || modelType.IsEnum)
-                return null;
+            if (modelType.IsValueType && 
+               (modelType.GetConstructor([typeof(Guid)]) != null || modelType.GetConstructor([typeof(Guid)]) != null))
+            {
+                return new BinderTypeModelBinder(typeof(StronglyTypedIdModelBinder));
+            }
 
-            // ✅ Use explicit binding flags to ensure we get the primary constructor property
-            var props = modelType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-            var hasSingleValueProperty = props.Length == 1 &&
-                (props[0].PropertyType == typeof(Guid) || props[0].PropertyType == typeof(string));
-
-            if (!hasSingleValueProperty)
-                return null;
-
-            return new BinderTypeModelBinder(typeof(StronglyTypedIdModelBinder));
+            return null;
         }
     }
 }
