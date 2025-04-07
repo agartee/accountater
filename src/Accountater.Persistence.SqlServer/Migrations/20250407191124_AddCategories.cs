74165755e34f2d84ea814f85c10e60e3d48c69d5
Adam Gartee <agartee@gmail.com>
2025-04-07 15:12:26 -0400

    added migration for category

---
+++ src/Accountater.Persistence.SqlServer/Migrations/20250407191124_AddCategories.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accountater.Persistence.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CategoryId",
+                schema: "Accountater",
+                table: "Transaction",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                schema: "Accountater",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transaction_CategoryId",
+                schema: "Accountater",
+                table: "Transaction",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_Name",
+                schema: "Accountater",
+                table: "Category",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transaction_Category_CategoryId",
+                schema: "Accountater",
+                table: "Transaction",
+                column: "CategoryId",
+                principalSchema: "Accountater",
+                principalTable: "Category",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transaction_Category_CategoryId",
+                schema: "Accountater",
+                table: "Transaction");
+
+            migrationBuilder.DropTable(
+                name: "Category",
+                schema: "Accountater");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transaction_CategoryId",
+                schema: "Accountater",
+                table: "Transaction");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                schema: "Accountater",
+                table: "Transaction");
+        }
+    }
+}
